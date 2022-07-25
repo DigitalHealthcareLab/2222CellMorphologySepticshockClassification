822e3db9af20cc8f19c6b51a041c167ddba61175
kimjh0107 <kimjh971717@gmail.com>
2022-07-26 08:44:43 +0900

    add path for test path

--- src/path.py
+++ src/path.py
@@ -13,3 +13,11 @@ def get_blind_pathes(remove_patient:int, celltype:str):
     cd8_x_test_path = get_blind_path(remove_patient, celltype, 'x', 'test')
     cd8_y_test_path = get_blind_path(remove_patient, celltype, 'y', 'test')
     return cd8_x_train_path, cd8_y_train_path, cd8_x_valid_path, cd8_y_valid_path, cd8_x_test_path, cd8_y_test_path
+
+def get_test_path(remove_patient:int, cell_type:str, target:str, type:str):
+    return Path(f'data/blind_test_{remove_patient}/test/{cell_type}_{target}_blind_{type}.npy')
+
+def get_blind_test_pathes(remove_patient:int, celltype:str):
+    test_cd8_x_test_path = get_test_path(remove_patient, celltype, 'x', 'test')
+    test_cd8_y_test_path = get_test_path(remove_patient, celltype, 'y', 'test')
+    return test_cd8_x_test_path, test_cd8_y_test_path
