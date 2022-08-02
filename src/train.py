35163f67f974d0cb9978c6a2c810d2b018add7cc
kimjh0107 <kimjh971717@gmail.com>
2022-08-03 08:32:49 +0900

    runner

--- src/train.py
+++ src/train.py
@@ -3,167 +3,8 @@ import copy
 import torch 
 import numpy as np 
 import matplotlib.pyplot as plt 
-import time 
-from src.optimizer import create_scheduler
-from src.model_densenet import create_regression_ANN
-from src.optimizer import get_optim
 from config import * 
-from src.device import get_device
 from sklearn.metrics import roc_auc_score
-
-
-
-def train_model_v1(model, dataloaders, criterion, optimizer, num_epochs, device, early_stopping,scheduler ,logger, scaler):
-    train_loss_history = []
-    val_loss_history = []
-
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_val_loss = 10000000
-
-   # running_loss = 0.0
-    for epoch in range(num_epochs):
-        print(f'Epoch {epoch} / {num_epochs}')
-        print('-'*10)
-
-        for phase in ['train', 'valid']:
-            if phase == 'train':
-                model.train()
-            else:
-                model.eval()
-            
-            running_loss = 0.0
-
-            # Iterate over data 
-            for data, targets in dataloaders[phase]:
-                data = data.to(device, non_blocking=True)
-                targets = targets.to(device, non_blocking=True)
-
-                optimizer.zero_grad(set_to_none=True)
-
-                with torch.cuda.amp.autocast():
-                    # forward 
-                    with torch.set_grad_enabled(phase == 'train'):
-                        scores = model(data)
-                        loss = criterion(scores,targets)
-
-                        if phase == ' train':                          
-                            scaler.scale(loss).backward()
-                            scaler.step(optimizer)
-                            scaler.update()
-                            scheduler.step()
-
-
-
-                running_loss += loss.item() * data.size(0)
-            epoch_loss = running_loss / len(dataloaders[phase].dataset)
-           # epoch_loss = running_loss / len(dataloaders[phase])
-            print('{} Loss: {:.4f}'.format(phase, epoch_loss))
-
-            # deep copy the model 
-            if phase == 'train':
-                train_loss_history.append(epoch_loss)
-            elif phase == 'valid':
-                val_loss_history.append(epoch_loss)
-
-                early_stopping(epoch, epoch_loss, model)
-
-                if epoch_loss < best_val_loss:
-                    best_val_loss = epoch_loss
-                    best_model_wts = copy.deepcopy(model.state_dict())
-
-        # if early_stopping.early_stop:
-        #     logger.info("Early Stopping")
-        #     break
-                 
-
-        print()
-
-    print('Best val Loss: {:4f}'.format(best_val_loss))   
-
-    model.load_state_dict(best_model_wts)
-    return model, train_loss_history, val_loss_history
-                    
-
-
-
-
-
-
-
-def train_model_test(model, dataloaders, criterion, optimizer, num_epochs, device, early_stopping,scheduler ,logger):
-    train_loss_history = []
-    val_loss_history = []
-
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_val_loss = 10000000
-
-   # running_loss = 0.0
-    for epoch in range(num_epochs):
-        print(f'Epoch {epoch} / {num_epochs}')
-        print('-'*10)
-
-        for phase in ['train', 'valid']:
-            if phase == 'train':
-                model.train()
-            else:
-                model.eval()
-            
-            running_loss = 0.0
-            import torch.nn as nn
-            # Iterate over data 
-            for data, targets in dataloaders[phase]:
-                data = data.to(device, non_blocking=True)
-                targets = targets.to(device, non_blocking=True)
-            #    targets = targets.unsqueeze(1)
-
-                # zero paramter gradients 
-                optimizer.zero_grad(set_to_none=True)
-
-                # forward -> track history if only in train 
-                with torch.set_grad_enabled(phase == 'train'):
-                    scores = model(data)
-                    loss = criterion(scores, targets) # calculate loss 
-
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
-                scheduler.step()
-                
-                
-                # statistics 
-                running_loss += loss.item() * data.size(0)
-            epoch_loss = running_loss / len(dataloaders[phase].dataset)
-           # epoch_loss = running_loss / len(dataloaders[phase])
-            print('{} Loss: {:.4f}'.format(phase, epoch_loss))
-
-            # deep copy the model 
-            if phase == 'train':
-                train_loss_history.append(epoch_loss)
-            elif phase == 'valid':
-                val_loss_history.append(epoch_loss)
-
-                early_stopping(epoch, epoch_loss, model)
-
-                if epoch_loss < best_val_loss:
-                    best_val_loss = epoch_loss
-                    best_model_wts = copy.deepcopy(model.state_dict())
-
-        # if early_stopping.early_stop:
-        #     logger.info("Early Stopping")
-        #     break
-                 
-
-        print()
-
-    print('Best val Loss: {:4f}'.format(best_val_loss))   
-
-    model.load_state_dict(best_model_wts)
-    return model, train_loss_history, val_loss_history
-                  
-
-
-
-
 
 
 def save_loss_plot(train_loss:np.array, val_loss:np.array, path):
@@ -176,17 +17,14 @@ def save_loss_plot(train_loss:np.array, val_loss:np.array, path):
 
 
 
-
-
-
 def run_batch(phase, model, criterion, optimizer, X, label, device) : 
     X = X.to(device, non_blocking = True)
     label = label.to(device , non_blocking= True)
-    #output, *_ = model(X)
-    output  = model(X)
+    output, *_ = model(X)
+    #output  = model(X)
 
 
-    loss = criterion(output, label)  # *** 
+    loss = criterion(output, label) 
     
     if phase == 'train' : 
         optimizer.zero_grad()
