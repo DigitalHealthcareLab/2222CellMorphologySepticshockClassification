613098d47bbd971d1f077a7565bbe6b557d39325
kimjh0107 <kimjh971717@gmail.com>
2022-08-10 09:21:35 +0900

    cuda

--- src/device.py
+++ src/device.py
@@ -1,7 +1,4 @@
 import torch 
 
-# def get_device():
-#     return torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-
 def get_device():
     return torch.device("cuda" if torch.cuda.is_available() else "cpu")
