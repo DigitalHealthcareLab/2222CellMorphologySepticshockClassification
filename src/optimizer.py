9837c1886dc8885cc1f61744420175d14296f5c0
kimjh0107 <kimjh971717@gmail.com>
2022-08-10 09:23:05 +0900

    SGD & Adam

--- src/optimizer.py
+++ src/optimizer.py
@@ -12,14 +12,6 @@ def get_optim(model, learning_rate, weight_decay, adam_epsilon):
                       weight_decay = weight_decay,
                       momentum=0.9)
 
-# def get_optim(model, learning_rate, weight_decay, adam_epsilon):
-#     return optim.Adagrad(model.parameters(), 
-#                       lr = learning_rate, 
-#                       weight_decay = weight_decay,
-#                       eps = adam_epsilon)
-
-
-
 def create_scheduler(optimizer):
     return optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=50, eta_min=0)
 
